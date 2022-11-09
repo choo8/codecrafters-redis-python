cd2401f821142ca65a56ab0a3566cfbca025b019
choo8 <choo8@illinois.edu>
2022-11-08 22:14:44 -0800

    Completed Stage 2

--- app/main.py
+++ app/main.py
@@ -9,8 +9,19 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
 
+    with conn:
+        while True:
+            data = conn.recv(1024)
+
+            if data[:2] != b"*1":
+                _, *client_input = data.split(" ")
+                resp = f"+{' '.join(client_input)}\r\n"
+            else:
+                resp = b"+PONG\r\n"
+            
+            conn.sendall(resp)
 
 if __name__ == "__main__":
     main()
